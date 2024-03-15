25ddf4db9ff2a165a0b0983b4084d91c76aaf0a0
sai shiva hari prasad <esshariprasad@gmail.com>
2024-03-15 08:57:57 -0500

    works in chrome not in edge

--- script.js
+++ script.js
@@ -1,170 +1,140 @@
-    // document.addEventListener('DOMContentLoaded', () => {
-    //   const voiceInput = document.getElementById('voiceInput');
-    //   const voiceButton = document.getElementById('voiceButton');
+        // document.addEventListener('DOMContentLoaded', () => {
+        //   const voiceInput = document.getElementById('voiceInput');
+        //   const voiceButton = document.getElementById('voiceButton');
 
 
-    //   voiceInput.addEventListener('click', () => {
-    //     voiceInput.removeAttribute('readonly');
-    //   });
-    // });
-
-    var ignore_onend;
-    var recognizing = false;
-
-    var final_transcript = '';
+        //   voiceInput.addEventListener('click', () => {
+        //     voiceInput.removeAttribute('readonly');
+        //   });
+        // });
 
 
-    var first_char = /\S/;
-function capitalize(s) {
-  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+
+var ignore_onend;
+var recognizing = false;
+
+var final_transcript = '';
+browser=''
+
+
+        var first_char = /\S/;
+    function capitalize(s) {
+    return s.replace(first_char, function(m) { return m.toUpperCase(); });
+    }
+
+if (!('webkitSpeechRecognition' in window)) {
+    browser="chrome browser not supported"
+    console.log("chrome browser not supported")
+
+    console.log("this browser doesnot support chrome voice input")
+} 
+
+else if ('SpeechRecognition' in window) {
+    // Firefox
+    recognition = new SpeechRecognition();
+
+    browser="firefox"
+    console.log("firefox")
+
+
+}
+
+else{
+
+    var recognition = new webkitSpeechRecognition();
+    browser="chrome browser"
+    console.log("chrome browser support")
+
 }
 
 
-    const startButton=()=>{
-            const recognition = new webkitSpeechRecognition(); // Use the appropriate interface for different browsers
+const startButton=()=>{
+        
+    if(recognition){
+        console.log(browser)
 
-            // console.log("inside the browser")
-            recognition.continuous = true;
-            recognition.interimResults = true;
-    
-            recognition.onresult = (event) => {
-            // console.log(event)
-            //   const spokenText = event.results[0][0].transcript;
-             
-
-              var interim_transcript = '';
-              for (var i = event.resultIndex; i < event.results.length; ++i) {
-                if (event.results[i].isFinal) {
-                  final_transcript += event.results[i][0].transcript;
-                } else {
-                  interim_transcript += event.results[i][0].transcript;
-                }
-              }
-              final_transcript = capitalize(final_transcript);
-              voiceInput.value = final_transcript;
-  
-
-
-
-            };
-    
-            if (recognizing) {
-                recognition.stop();
-                // console.log("stoping the recording")
-                start_img.src = 'mic.gif';
-                return;
-              }
-
-            recognition.start();
-
-            recognition.onstart = function() {
-                recognizing = true;
-                console.log("starting")
-                start_img.src = 'mic-animate.gif';
-              };
-
-              recognition.onerror = function(event) {
-                if (event.error == 'no-speech') {
-                  start_img.src = 'mic.gif';
-                  console.log("error")    
-                  ignore_onend = true;
-                }
-                if (event.error == 'audio-capture') {
-                  start_img.src = 'mic.gif';
-                 console.log("audio capture error")
-                  ignore_onend = true;
-                }
-                if (event.error == 'not-allowed') {
-                  if (event.timeStamp - start_timestamp < 100) {
-                    // console.log("info blocked")
-                  } else {
-                    
-                  }
-                  ignore_onend = true;
-                }
-              };
-
-              recognition.onend = function() {
-                // console.log("in recognition on end")
-                recognizing = false;
-                if (ignore_onend) {
-                  return;
-                }
-                start_img.src = 'mic.gif';
-
-
-              };
-
-    }
-
-  
-    if (!('webkitSpeechRecognition' in window)) {
-
-        console.log("this browser doesnot support chrome voice input")
-      } 
-
-      else{
-
-        var recognition = new webkitSpeechRecognition();
+        // console.log("inside the browser")
         recognition.continuous = true;
         recognition.interimResults = true;
 
+        recognition.onresult = (event) => {
+        // console.log(event)
+        //   const spokenText = event.results[0][0].transcript;
+        
+
+        var interim_transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+            } else {
+            interim_transcript += event.results[i][0].transcript;
+            }
+        }
+        final_transcript = capitalize(final_transcript);
+        voiceInput.value = final_transcript;
+
+
+
+
+        };
+
+        if (recognizing) {
+            recognition.stop();
+            // console.log("stoping the recording")
+            start_img.src = 'mic.gif';
+            return;
+        }
+
+        recognition.start();
+
         recognition.onstart = function() {
             recognizing = true;
-            showInfo('info_speak_now');
+            console.log("starting")
             start_img.src = 'mic-animate.gif';
-          };
+        };
 
-
-
-          recognition.onerror = function(event) {
+        recognition.onerror = function(event) {
             if (event.error == 'no-speech') {
-              start_img.src = 'mic.gif';
-            //   console.log("error")    
-              ignore_onend = true;
+            start_img.src = 'mic.gif';
+            console.log("error")    
+            ignore_onend = true;
             }
             if (event.error == 'audio-capture') {
-              start_img.src = 'mic.gif';
-            //  console.log("audio capture error")
-              ignore_onend = true;
+            start_img.src = 'mic.gif';
+            console.log("audio capture error")
+            ignore_onend = true;
             }
             if (event.error == 'not-allowed') {
-              if (event.timeStamp - start_timestamp < 100) {
+            if (event.timeStamp - start_timestamp < 100) {
                 // console.log("info blocked")
-              } else {
+            } else {
                 
-              }
-              ignore_onend = true;
             }
-          };
+            ignore_onend = true;
+            }
+        };
 
-
-          recognition.onend = function() {
+        recognition.onend = function() {
+            // console.log("in recognition on end")
             recognizing = false;
             if (ignore_onend) {
-              return;
+            return;
             }
             start_img.src = 'mic.gif';
-            if (!final_transcript) {
-              showInfo('info_start');
-              return;
-            }
-            showInfo('');
-            if (window.getSelection) {
-              window.getSelection().removeAllRanges();
-              var range = document.createRange();
-              range.selectNode(document.getElementById('final_span'));
-              window.getSelection().addRange(range);
-            }
-            if (create_email) {
-              create_email = false;
-              createEmail();
-            }
-          };
-        
-          recognition.onresult = (event) => {
-            const spokenText = event.results[0][0].transcript;
-            voiceInput.value = spokenText;
-          };
-  
-      }
+
+
+        };
+    }
+    else{
+        console.log("voice input not supported")
+    }
+
+}
+
+    
+
+
+
+
+
+
